0370b8af30ffa064e11efb5f833acf08ccf24f5a
kahlow <tkahlow@gmail.com>
2013-08-12 13:22:15 -0400

    fixed slide down 2.0

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
 	$('.undernav ul li a').hoverIntent({
 		over: function () {
-			$('.menudropdown').slideDown(200, function (){
-				$('.menudropdown').show();
+			$('.menudropdown').slideDown(200, function () {
+				$('.menudropdown', this).show();
 			});
 		},
-		out: function () {
-			$('.menudropdown').hide();
-			$('.menudropdown', this).slideUp(200);
+		out: function () {			
+			$('.menudropdown').slideUp(200, function () {
+				$('.menudropdown', this).hide();
+			});
 		},
 		timeout: 200
 	});
