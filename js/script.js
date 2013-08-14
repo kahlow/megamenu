f5e5dd49a1ca41b8fe784e7a634c05734af85792
kahlow <tkahlow@gmail.com>
2013-08-14 09:16:55 -0400

    added better hover states

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
 $(document).ready(function () {
-	$('.undernav ul li a').hoverIntent({
+	$('.undernav ul li').hoverIntent({
 		over: function () {
-			$('.menudropdown').slideDown(200, function () {
-				$('.menudropdown', this).show();
-			});
-		},
-		out: function () {			
-			$('.menudropdown').slideUp(200, function () {
-				$('.menudropdown', this).hide();
-			});
-		},
-		timeout: 200
+			// if not visible
+			if (!$('.menudropdown').is(':visible')) {
+				$('.menudropdown').slideDown(200, function () {
+					$('.menudropdown').show();
+				});
+			}
+			else {
+				// if visible, slide up and slide down new menu
+				$('.menudropdown').slideUp('fast', function () {
+					$('.menudropdown').hide();
+				});
+				$('.menudropdown').slideDown(200, function () {
+					$('.menudropdown').show();
+				});
+			}
+		}
 	});
+
+	$('.menudropdown').hover(
+		function () {
+
+		},
+		function () {
+			$('.menudropdown').slideUp('fast', function () {
+				$('.menudropdown').hide();
+			});
+		}
+	);
 });
